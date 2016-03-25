974579cae465a6abf525305956bd74208242693e
Peng Hu <penghu_scu@163.com>
2016-03-25 20:31:46 +0800

    fuck

--- slice_test_multi_2_2.m
+++ slice_test_multi_2_2.m
@@ -30,34 +30,34 @@ parfor t = 1 : times
 
         w2 = [w1 randi(2, 1, num_watermark) - 1];
         y21 = slice_encode(x, w2, k, a, n(1 : 4), 10);
-        mp3write(y21, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y22, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        mp3write(y21, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y22, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
         y21 = 1.8 * y21;
         w21 = slice_decode(y21, length(w2), k, n(1 : 4), 10);
         w22 = slice_decode(y22, length(w2), k, n(1 : 4), 10);
 
         w3 = [w2 randi(2, 1, num_watermark) - 1];
         y61 = echo_encode(x, w3, k, a, n(1 : 2));
-        mp3write(y61, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y62, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        mp3write(y61, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y62, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
         y61 = 1.8 * y61;
         w61 = echo_decode(y61, length(w3), k, n(1 : 2));
         w62= echo_decode(y62, length(w3), k, n(1 : 2));
         
         y31 = slice_encode(x, w3, k, a, n(1 : 8), 10);
-        mp3write(y31, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y32, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        mp3write(y31, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y32, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
         y31 = 1.8 * y31;
         w31 = slice_decode(y31, length(w3), k, n(1 : 8), 10);
         w32 = slice_decode(y32, length(w3), k, n(1 : 8), 10);
 
         w4 = [w3 randi(2, 1, num_watermark) - 1];
         y41 = slice_encode(x, w4, k, a, n(1 : 16), 10);
-        mp3write(y41, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y42, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        mp3write(y41, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y42, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
         y41 = 1.8 * y41;
         w41 = slice_decode(y41, length(w4), k, n(1 : 16), 10);
         w42 = slice_decode(y42, length(w4), k, n(1 : 16), 10);
 
         w5 = [w4 randi(2, 1, num_watermark) - 1];
         y51 = slice_encode(x, w5, k, a, n(1 : 32), 10);
-        mp3write(y51, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y52, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        mp3write(y51, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y52, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
         y51 = 1.8 * y51;
         w51 = slice_decode(y51, length(w5), k, n(1 : 32), 10);
         w52 = slice_decode(y52, length(w5), k, n(1 : 32), 10);
