aa9025a39a68816afc951b4299b5ab25d2480d37
Peng Hu <penghu_scu@163.com>
2016-03-25 20:24:55 +0800

    multi slice 2

--- slice_test_multi_2_2.m
+++ slice_test_multi_2_2.m
@@ -13,63 +13,56 @@ a = 0.002;
 
 result = cell(times, 1);
 for t = 1 : times
-    result{t} = zeros(3, 6);
+    result{t} = zeros(2, 6);
 end
 % count_water = 0;
-parfor t = 1 : times
+for t = 1 : times
     for i = 1 : count
         [x, fs] = audioread(strcat(files_root_path, all_files(i).name));
         k = PNSequence(Lpn);
         
         w1 = randi(2, 1, num_watermark) - 1;
         y11 = echo_encode(x, w1, k, a, n(1 : 2));
-        y12 = wav_quantize(y11, 8);
-        y13 = awgn(y11, 30, 'measured');
+        mp3write(y11, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y12, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));
+        y11 = 1.8 * y11;
         w11 = echo_decode(y11, length(w1), k, n(1 : 2));
         w12= echo_decode(y12, length(w1), k, n(1 : 2));
-        w13 = echo_decode(y13, length(w1), k, n(1 : 2));
 
         w2 = [w1 randi(2, 1, num_watermark) - 1];
         y21 = slice_encode(x, w2, k, a, n(1 : 4), 10);
-        y22 = wav_quantize(y21, 8);
-        y23 = awgn(y21, 30, 'measured');
+        mp3write(y21, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y22, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        y21 = 1.8 * y21;
         w21 = slice_decode(y21, length(w2), k, n(1 : 4), 10);
         w22 = slice_decode(y22, length(w2), k, n(1 : 4), 10);
-        w23 = slice_decode(y23, length(w2), k, n(1 : 4), 10);
 
         w3 = [w2 randi(2, 1, num_watermark) - 1];
         y61 = echo_encode(x, w3, k, a, n(1 : 2));
-        y62 = wav_quantize(y61, 8);
-        y63 = awgn(y61, 30, 'measured');
+        mp3write(y61, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y62, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        y61 = 1.8 * y61;
         w61 = echo_decode(y61, length(w3), k, n(1 : 2));
         w62= echo_decode(y62, length(w3), k, n(1 : 2));
-        w63 = echo_decode(y63, length(w3), k, n(1 : 2));
         
         y31 = slice_encode(x, w3, k, a, n(1 : 8), 10);
-        y32 = wav_quantize(y31, 8);
-        y33 = awgn(y31, 30, 'measured');
+        mp3write(y31, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y32, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        y31 = 1.8 * y31;
         w31 = slice_decode(y31, length(w3), k, n(1 : 8), 10);
         w32 = slice_decode(y32, length(w3), k, n(1 : 8), 10);
-        w33 = slice_decode(y33, length(w3), k, n(1 : 8), 10);
 
         w4 = [w3 randi(2, 1, num_watermark) - 1];
         y41 = slice_encode(x, w4, k, a, n(1 : 16), 10);
-        y42 = wav_quantize(y41, 8);
-        y43 = awgn(y41, 30, 'measured');
+        mp3write(y41, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y42, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        y41 = 1.8 * y41;
         w41 = slice_decode(y41, length(w4), k, n(1 : 16), 10);
         w42 = slice_decode(y42, length(w4), k, n(1 : 16), 10);
-        w43 = slice_decode(y43, length(w4), k, n(1 : 16), 10);
 
         w5 = [w4 randi(2, 1, num_watermark) - 1];
-        y51 = slice_encode(x, w5, k, a, n(1 : 2), 32);
-        y52 = wav_quantize(y51, 8);
-        y53 = awgn(y51, 30, 'measured');
+        y51 = slice_encode(x, w5, k, a, n(1 : 32), 10);
+        mp3write(y51, fs, strcat('tmp/', num2str(t), 'tmp.mp3')); [y52, fs] = mp3read(strcat('tmp/', num2str(t), 'tmp.mp3'));        y53 = awgn(y51, 30, 'measured');
+        y51 = 1.8 * y51;
         w51 = slice_decode(y51, length(w5), k, n(1 : 32), 10);
         w52 = slice_decode(y52, length(w5), k, n(1 : 32), 10);
-        w53 = slice_decode(y53, length(w5), k, n(1 : 32), 10);
         result{t} = result{t} + [sum(w1 == w11) sum(w3 == w61) sum(w2 == w21) sum(w3 == w31) sum(w4 == w41) sum(w5 == w51);
-                                 sum(w1 == w12) sum(w3 == w62) sum(w2 == w22) sum(w3 == w32) sum(w4 == w42) sum(w5 == w52);
-                                 sum(w1 == w13) sum(w3 == w63) sum(w2 == w23) sum(w3 == w33) sum(w4 == w43) sum(w5 == w53)];
+                                 sum(w1 == w12) sum(w3 == w62) sum(w2 == w22) sum(w3 == w32) sum(w4 == w42) sum(w5 == w52)];
         if mod(i, 50) == 0
             disp([t i]);
             disp(result{t} / (i * num_watermark) * 100);
@@ -78,4 +71,5 @@ parfor t = 1 : times
 end
 ret = netsum(result) / (count * times * num_watermark) * 100;
 disp(ret);
-save('/home/wujing/hupeng/slice_test_multi_2_1.mat');
+save('/home/wujing/hupeng/slice_test_multi_2_2.mat');
+
