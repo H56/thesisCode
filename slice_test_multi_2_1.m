3425eefc6381940f3d2a04f85612b3ca6ca36fe9
Peng Hu <penghu_scu@163.com>
2016-03-25 19:51:57 +0800

    change multi slice dimension erro

--- slice_test_multi_2_1.m
+++ slice_test_multi_2_1.m
@@ -21,7 +21,7 @@ parfor t = 1 : times
         [x, fs] = audioread(strcat(files_root_path, all_files(i).name));
         k = PNSequence(Lpn);
         
-        w = randi(2, 1, num_watermark) - 1;
+        w1 = randi(2, 1, num_watermark) - 1;
         y11 = echo_encode(x, w, k, a, n(1 : 2));
         y12 = wav_quantize(y11, 8);
 	    y13 = awgn(y11, 30, 'measured');
@@ -29,7 +29,7 @@ parfor t = 1 : times
         w12= echo_decode(y12, num_watermark, k, n(1 : 2));
         w13 = echo_decode(y13, num_watermark, k, n(1 : 2));
 
-        w = [w randi(2, 1, num_watermark) - 1];
+        w2 = [w randi(2, 1, num_watermark) - 1];
         y61 = echo_encode(x, w, k, a, n(1 : 2));
         y62 = wav_quantize(y61, 8);
 	    y63 = awgn(y61, 30, 'measured');
@@ -44,7 +44,7 @@ parfor t = 1 : times
         w22 = slice_decode(y22, num_watermark, k, n(1 : 4), 10);
         w23 = slice_decode(y23, num_watermark, k, n(1 : 4), 10);
 
-        w = [w randi(2, 1, num_watermark) - 1];
+        w3 = [w randi(2, 1, num_watermark) - 1];
         y31 = slice_encode(x, w, k, a, n(1 : 8), 10);
         y32 = wav_quantize(y31, 8);
 	    y33 = awgn(y31, 30, 'measured');
@@ -52,7 +52,7 @@ parfor t = 1 : times
         w32 = slice_decode(y32, num_watermark, k, n(1 : 8), 10);
         w33 = slice_decode(y33, num_watermark, k, n(1 : 8), 10);
 
-        w = [w randi(2, 1, num_watermark) - 1];
+        w4 = [w randi(2, 1, num_watermark) - 1];
         y41 = slice_encode(x, w, k, a, n(1 : 16), 10);
         y42 = wav_quantize(y41, 8);
 	    y43 = awgn(y41, 30, 'measured');
@@ -60,16 +60,16 @@ parfor t = 1 : times
         w42 = slice_decode(y42, num_watermark, k, n(1 : 16), 10);
         w43 = slice_decode(y43, num_watermark, k, n(1 : 16), 10);
 
-        w = [w randi(2, 1, num_watermark) - 1];
+        w5 = [w randi(2, 1, num_watermark) - 1];
         y51 = slice_encode(x, w, k, a, n(1 : 2), 32);
         y52 = wav_quantize(y51, 8);
 	    y53 = awgn(y51, 30, 'measured');
         w51 = slice_decode(y51, num_watermark, k, n(1 : 32), 10);
         w52 = slice_decode(y52, num_watermark, k, n(1 : 32), 10);
         w53 = slice_decode(y53, num_watermark, k, n(1 : 32), 10);
-        result{t} = result{t} + [sum(w == w11) sum(w == w61) sum(w == w21) sum(w == w31) sum(w == w41) sum(w == w51);
-                                 sum(w == w12) sum(w == w62) sum(w == w22) sum(w == w32) sum(w == w42) sum(w == w52);
-                                 sum(w == w13) sum(w == w63) sum(w == w23) sum(w == w33) sum(w == w43) sum(w == w53)];
+        result{t} = result{t} + [sum(w1 == w11) sum(w2 == w61) sum(w2 == w21) sum(w3 == w31) sum(w4 == w41) sum(w5 == w51);
+                                 sum(w1 == w12) sum(w2 == w62) sum(w2 == w22) sum(w3 == w32) sum(w4 == w42) sum(w5 == w52);
+                                 sum(w1 == w13) sum(w2 == w63) sum(w2 == w23) sum(w3 == w33) sum(w4 == w43) sum(w5 == w53)];
         if mod(i, 50) == 0
             disp([t i]);
             disp(result{t} / (i * num_watermark) * 100);
