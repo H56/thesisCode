516d73b13b378ede4bad3413e3c844a1e12cb2c6
Peng Hu <penghu_scu@163.com>
2016-05-03 22:24:31 +0800

    slice ep

--- slice_test_multi_2_1.m
+++ slice_test_multi_2_1.m
@@ -7,7 +7,7 @@ count = length(all_files);
 times = 100;
 n = [20 40 60 80 100 120 140 160 180 200 220 240 260 280 300 320
     340 360 380 400 420 440 460 480 500 520 540 560 580 600 620 640];
-num_watermark = 8;
+num_watermark = 40;
 Lpn = 1023;
 a = 0.002;
 
@@ -29,47 +29,50 @@ parfor t = 1 : times
         w12= echo_decode(y12, length(w1), k, n(1 : 2));
         w13 = echo_decode(y13, length(w1), k, n(1 : 2));
 
-        w2 = [w1 randi(2, 1, num_watermark) - 1];
-        y21 = slice_encode(x, w2, k, a, n(1 : 4), 8);
+%         w2 = [w1 randi(2, 1, num_watermark) - 1];
+        w2 = w1;
+        y21 = ep_encode(x, w, k, a3, n(1 : 2));
         y22 = wav_quantize(y21, 8);
         y23 = awgn(y21, 30, 'measured');
-        w21 = slice_decode(y21, length(w2), k, n(1 : 4), 8);
-        w22 = slice_decode(y22, length(w2), k, n(1 : 4), 8);
-        w23 = slice_decode(y23, length(w2), k, n(1 : 4), 8);
+        w21 = ep_decode(y21, num_watermark, k, n(1 : 2));
+        w22 = ep_decode(y22, num_watermark, k, n(1 : 2));
+        w23 = ep_decode(y23, num_watermark, k, n(1 : 2));
 
-        w3 = [w2 randi(2, 1, num_watermark) - 1];
-        y61 = echo_encode(x, w3, k, a, n(1 : 2));
-        y62 = wav_quantize(y61, 8);
-        y63 = awgn(y61, 30, 'measured');
-        w61 = echo_decode(y61, length(w3), k, n(1 : 2));
-        w62= echo_decode(y62, length(w3), k, n(1 : 2));
-        w63 = echo_decode(y63, length(w3), k, n(1 : 2));
+%         w3 = [w2 randi(2, 1, num_watermark) - 1];
+%         w3 = w1;
+%         y61 = echo_encode(x, w3, k, a, n(1 : 2));
+%         y62 = wav_quantize(y61, 8);
+%         y63 = awgn(y61, 30, 'measured');
+%         w61 = echo_decode(y61, length(w3), k, n(1 : 2));
+%         w62= echo_decode(y62, length(w3), k, n(1 : 2));
+%         w63 = echo_decode(y63, length(w3), k, n(1 : 2));
         
-        y31 = slice_encode(x, w3, k, a, n(1 : 8), 8);
+        y31 = slice_encode(x, w3, k, a, n(1 : 16), 8);
         y32 = wav_quantize(y31, 8);
         y33 = awgn(y31, 30, 'measured');
-        w31 = slice_decode(y31, length(w3), k, n(1 : 8), 8);
-        w32 = slice_decode(y32, length(w3), k, n(1 : 8), 8);
-        w33 = slice_decode(y33, length(w3), k, n(1 : 8), 8);
+        w31 = slice_decode(y31, length(w3), k, n(1 : 16), 8);
+        w32 = slice_decode(y32, length(w3), k, n(1 : 16), 8);
+        w33 = slice_decode(y33, length(w3), k, n(1 : 16), 8);
 
-        w4 = [w3 randi(2, 1, num_watermark) - 1];
-        y41 = slice_encode(x, w4, k, a, n(1 : 16), 8);
+%         w4 = [w3 randi(2, 1, num_watermark) - 1];
+        w4 = w1;
+        y41 = slice_encode(x, w4, k, a, n(1 : 32), 8);
         y42 = wav_quantize(y41, 8);
         y43 = awgn(y41, 30, 'measured');
-        w41 = slice_decode(y41, length(w4), k, n(1 : 16), 8);
-        w42 = slice_decode(y42, length(w4), k, n(1 : 16), 8);
-        w43 = slice_decode(y43, length(w4), k, n(1 : 16), 8);
+        w41 = slice_decode(y41, length(w4), k, n(1 : 32), 8);
+        w42 = slice_decode(y42, length(w4), k, n(1 : 32), 8);
+        w43 = slice_decode(y43, length(w4), k, n(1 : 32), 8);
 
-        w5 = [w4 randi(2, 1, num_watermark) - 1];
+        w5 = [w4 randi(2, 1, 10) - 1];
         y51 = slice_encode(x, w5, k, a, n(1 : 32), 8);
         y52 = wav_quantize(y51, 8);
         y53 = awgn(y51, 30, 'measured');
         w51 = slice_decode(y51, length(w5), k, n(1 : 32), 8);
         w52 = slice_decode(y52, length(w5), k, n(1 : 32), 8);
         w53 = slice_decode(y53, length(w5), k, n(1 : 32), 8);
-        result{t} = result{t} + [sum(w1 == w11) sum(w3 == w61) sum(w2 == w21) sum(w3 == w31) sum(w4 == w41) sum(w5 == w51);
-                                 sum(w1 == w12) sum(w3 == w62) sum(w2 == w22) sum(w3 == w32) sum(w4 == w42) sum(w5 == w52);
-                                 sum(w1 == w13) sum(w3 == w63) sum(w2 == w23) sum(w3 == w33) sum(w4 == w43) sum(w5 == w53)];
+        result{t} = result{t} + [sum(w1 == w11) sum(w2 == w21) sum(w3 == w31) sum(w4 == w41) sum(w5 == w51);
+                                 sum(w1 == w12) sum(w2 == w22) sum(w3 == w32) sum(w4 == w42) sum(w5 == w52);
+                                 sum(w1 == w13) sum(w2 == w23) sum(w3 == w33) sum(w4 == w43) sum(w5 == w53)];
         if mod(i, 50) == 0
             disp([t i]);
             disp(bsxfun(@rdivide, result{t}, [1 3 2 3 4 5] * (i * num_watermark)) * 100);
